859059a5daf682d2b676d02b298fdfd0c7e2ece6
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2024-01-15 07:17:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ class Config(object):
     TECH_VJ_UPDATES_CHANNEL = int(tech_vj_update_channel) if tech_vj_update_channel and id_pattern.search(tech_vj_update_channel) else None  
     
     # Url Shortner Information 
-    TECH_VJ = bool(environ.get('TECH_VJ', False) # Set False If you want shortlink off else True
+    TECH_VJ = bool(environ.get('TECH_VJ', 'False') # Set False If you want shortlink off else True
     TECH_VJ_URL = environ.get('TECH_VJ_URL', 'moneykamalo.com') # your shortlink url domain or url without https://
     TECH_VJ_API = environ.get('TECH_VJ_API', '8a04025f212aa7c29f79bb1c2ac28300e149a7e0') # your url shortner api
     TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/How_To_Open_Linkl")
